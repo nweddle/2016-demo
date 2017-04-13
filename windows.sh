daaf6bc17a9c7eeeb8ef8d5830df5b7a77854da2
workstation-1 <workstation-1@automate-demo.com>
2017-04-13 06:18:55 +0000

    add roles

--- windows.sh
+++ windows.sh
@@ -1,1 +1,5 @@
 knife ec2 server create --fqdn WinDyn1 -g sg-03865f78 --subnet subnet-cef71695 -f m3.large -S nweddle_sa --image ami-94e26af4 --associate-public-ip --user-data win_user_data -N WinDyn1 -r 'role[webserver-win]' -i ~/.ssh/nweddle_sa.pem --winrm-user Administrator --winrm-password Ch4ng3m3 --winrm-transport plaintext
+
+knife ec2 server create --fqdn WinDyn2 -g sg-03865f78 --subnet subnet-cef71695 -f m3.large -S nweddle_sa --image ami-94e26af4 --associate-public-ip --user-data win_user_data -N WinDyn2 -r 'role[webserver-win]' -i ~/.ssh/nweddle_sa.pem --winrm-user Administrator --winrm-password Ch4ng3m3 --winrm-transport plaintext
+
+knife ec2 server create --fqdn WinDyn3 -g sg-03865f78 --subnet subnet-cef71695 -f m3.large -S nweddle_sa --image ami-94e26af4 --associate-public-ip --user-data win_user_data -N WinDyn3 -r 'role[webserver-win]' -i ~/.ssh/nweddle_sa.pem --winrm-user Administrator --winrm-password Ch4ng3m3 --winrm-transport plaintext
